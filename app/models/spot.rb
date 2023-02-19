fd1bf4b881f824b778fd38fb9ff8f172620dcd09
ghulamali16 <ghulam.ali@devsinc.com>
2023-02-19 15:42:11 +0500

    Initial commit

---
+++ app/models/spot.rb
@@ -0,0 +1,9 @@
+# app/models/spot.rb
+class Spot < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+end
+
